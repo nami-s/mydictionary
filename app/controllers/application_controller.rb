cf15df78973a8aa9c699e4a88712dfa46fece5bb
nami-s <na.s1222.10feettt@gmail.com>
2021-02-09 04:06:26 +0000

    [add]modal検索フォーム設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :search_product
 
   def after_sign_in_path_for(resource)
     root_path
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def search_product
+    @p = Article.ransack(params[:q])
+    @results = @p.result
+    @categories = Category.all
+  end
+
 
 end
