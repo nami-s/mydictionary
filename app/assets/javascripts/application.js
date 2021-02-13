935d2b43395e926ebad1ff29d19d0a5685570717
nami-s <na.s1222.10feettt@gmail.com>
2021-02-13 13:07:35 +0000

    [fix]application.jsファイル

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,26 +33,26 @@ document.addEventListener('turbolinks:load', () => {
   });
 });
 
-$(function () {
-  // 画面遷移を検知
-  $(document).on('turbolinks:load', function () {
-    // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
-    if ($('#calendar').length) {
-      function eventCalendar() {
-        return $('#calendar').fullCalendar({
-        });
-      }
-      function clearCalendar() {
-          $('#calendar').html('');
-      }
-      $(document).on('turbolinks:load', function () {
-          eventCalendar();
-      });
-      $(document).on('turbolinks:before-cache', clearCalendar);
+// $(function () {
+//   // 画面遷移を検知
+//   $(document).on('turbolinks:load', function () {
+//     // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+//     if ($('#calendar').length) {
+//       function eventCalendar() {
+//         return $('#calendar').fullCalendar({
+//         });
+//       }
+//       function clearCalendar() {
+//           $('#calendar').html('');
+//       }
+//       $(document).on('turbolinks:load', function () {
+//           eventCalendar();
+//       });
+//       $(document).on('turbolinks:before-cache', clearCalendar);
 
-      $('#calendar').fullCalendar({
-          schedules: '/schedules.json'
-      });
-    }
-  });
-});
+//       $('#calendar').fullCalendar({
+//           schedules: '/schedules.json'
+//       });
+//     }
+//   });
+// });
