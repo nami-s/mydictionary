affe3add85eaf0944f182925ce264c4473ac30b3
nami-s <na.s1222.10feettt@gmail.com>
2021-02-17 13:27:33 +0000

    [add]バッチ処理対象データ

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,6 +1,7 @@
 class Batch::DataReset
   def self.data_reset
     # 投稿を全て削除
+    User.find_by(isguest: true).schedules.delete_all
     User.find_by(isguest: true).categories.delete_all
     User.find_by(isguest: true).articles.delete_all
     p "投稿を全て削除しました"
