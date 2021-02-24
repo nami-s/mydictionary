c8cf27a17c78ac3246df29dfbeb33f077f9a5e0f
nami-s <na.s1222.10feettt@gmail.com>
2021-02-24 13:05:45 +0000

    [add]ransackソート機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,12 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @category_params = params[:q][:category_id_eq]
+    @date_params = params[:q][:created_at_eq]
+    @keyword_params = params[:q][:title_or_body_cont]
+  end
+
+  def search
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,10 +4,14 @@ class Article < ApplicationRecord
   belongs_to :category
   accepts_nested_attributes_for :category
   has_many :checks, dependent: :destroy
-  
-  attachment :image, content_type: ["image/jpeg", "image/png", "image/gif"]
-
+  attachment :image, content_type: ["image/jpeg", "image/png", "image/gif"] # 画像ファイルのみ選別。Refileのヘルパー
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true
+
+
+  # ransacker :check_count do
+  #   query = '(SELECT COUNT(checks.article_id) FROM checks where checks.article_id = article.id GROUP BY checks.article_id)'
+  #   Arel.sql(query)
+  # end
 
 end
