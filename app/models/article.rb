5013d13ff473258444f6099a5fbf7d9d8e42dda1
nami-s <na.s1222.10feettt@gmail.com>
2021-02-24 13:31:50 +0000

    [add]ransack閲覧回数順検索機能

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,15 +3,15 @@ class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
   accepts_nested_attributes_for :category
-  has_many :checks, dependent: :destroy
+  has_many :checks, dependent: :delete_all
   attachment :image, content_type: ["image/jpeg", "image/png", "image/gif"] # 画像ファイルのみ選別。Refileのヘルパー
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true
 
 
-  # ransacker :check_count do
-  #   query = '(SELECT COUNT(checks.article_id) FROM checks where checks.article_id = article.id GROUP BY checks.article_id)'
-  #   Arel.sql(query)
-  # end
+  ransacker :checks_count do
+    query = '(SELECT COUNT(checks.article_id) FROM checks where checks.article_id = articles.id GROUP BY checks.article_id)'
+    Arel.sql(query)
+  end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
 
   belongs_to :user
-  has_many :articles, dependent: :destroy
+  has_many :articles, dependent: :delete_all
 
   validates :name, presence: true, length: { maximum: 20 }
 
