bccba79044f1a496992007cecfd020de55c04ff8
nami-s <na.s1222.10feettt@gmail.com>
2021-03-21 06:42:48 +0000

    [fix]検索処理ヘルパーに移動/include

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :search_product, if: :current_user
+  include SearchesHelper
 
   def after_sign_in_path_for(resource)
     root_path
@@ -16,20 +17,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
-  def search_product
-    # 改行、半角全角スペースで区切られた箇所をハッシュにしている
-    # Parameters: {"q"=>{"sorts"=>"id asc", "created_at_eq"=>"", "category_id_eq"=>"12", "title_or_body_cont"=>""}, "commit"=>" 検索 "}
-    words = params[:q].delete(:title_or_body_cont) if params[:q].present?
-    if words.present?
-      params[:q][:groupings] = []
-      words.split(/[\f\n\r\t\v 　]/).each_with_index do |word, i|
-        params[:q][:groupings][i] = { title_or_body_cont: word }
-      end
-    end
-    @search = current_user.articles.ransack(params[:q])
-    @results = @search.result.page(params[:page]).per(15).order(" created_at DESC ")
-    @categories = current_user.categories.all
-  end
-
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def truncate_title(title)
+    title = strip_tags(title).truncate(9)
+  end
 end
